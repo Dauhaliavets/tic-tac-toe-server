4f17dd7eb9d5aa013235e537ac7daba4cfb49eb2
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-28 23:31:48 +0300

    fix: create & leave room

--- src/server.ts
+++ src/server.ts
@@ -17,33 +17,57 @@ const io = new Server(httpServer, {
   },
 });
 
-interface IRooms {
-  [key: string]: string[];
+interface IRoom {
+  name: string;
+  users: string[];
 }
 
-let rooms: IRooms = {};
+let rooms: IRoom[] = [];
 
 io.on('connection', (socket: Socket) => {
-  socket.on('signIn', async () => {
-    io.to(socket.id).emit('signInResponse', rooms);
+  socket.on('signIn', () => {
+    io.to(socket.id).emit('updateRooms', rooms);
   });
 
-  socket.on('selectRoom', async ({ room }: { room: string }) => {
-    socket.join(room);
-    const newRoomState = [...rooms[room], socket.id];
-    rooms[room] = newRoomState;
+  socket.on('createRoom', ({ roomName }: { roomName: string }) => {
+    rooms.push({ name: roomName, users: [socket.id] });
+    socket.join(roomName);
+    socket.broadcast.emit('updateRooms', rooms);
+    io.to(socket.id).emit('createdRoom', roomName);
+  });
+
+  socket.on('selectRoom', ({ roomName }: { roomName: string }) => {
+    const selectedRoom = rooms.findIndex((room) => room.name === roomName);
+    rooms[selectedRoom].users.push(socket.id);
+    socket.join(roomName);
     socket.broadcast.emit('updateRooms', rooms);
   });
 
-  socket.on('move', ({ board }) => {
-    const [room] = [...socket.rooms];
-    socket.to(room).emit('updateGame', board);
+  socket.on('leaveRoom', ({ roomName }: { roomName: string }) => {
+    const indSelectedRoom = rooms.findIndex((room) => room.name === roomName);
+    if (rooms[indSelectedRoom].users.length === 1) {
+      rooms.splice(indSelectedRoom, 1);
+    } else {
+      rooms[indSelectedRoom].users = rooms[indSelectedRoom].users.filter((user) => user !== socket.id);
+    }
+    socket.leave(roomName);
+    io.emit('updateRooms', rooms);
+  });
+
+  socket.on('move', ({ roomName, newBoard }) => {
+    socket.to(roomName).emit('updateGame', newBoard);
   });
 
   socket.on('disconnect', () => {
-    const [roomName] = [...socket.rooms];
-    rooms[roomName] = rooms[roomName].filter((userId) => userId !== socket.id);
-    socket.broadcast.emit('updateRooms', rooms);
+    const indRoomIncludesUser = rooms.findIndex((room) => room.users.includes(socket.id));
+    if (indRoomIncludesUser !== -1) {
+      if (rooms[indRoomIncludesUser].users.length === 1) {
+        rooms.splice(indRoomIncludesUser, 1);
+      } else {
+        rooms[indRoomIncludesUser].users = rooms[indRoomIncludesUser].users.filter((user) => user !== socket.id);
+      }
+      socket.broadcast.emit('updateRooms', rooms);
+    }
     socket.disconnect();
   });
 });
